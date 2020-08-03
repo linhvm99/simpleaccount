157585d62e1bc57f78102f81942beba3e1a03e1c
linhvm99 <linhvm99@gmail.com>
2020-08-03 13:50:08 +0700

    Responsive for mobile

--- script.js
+++ script.js
@@ -47,3 +47,56 @@ function deleteAll() {
     var table = document.getElementById("tbody")
     table.innerHTML = ""
 }
+
+$(document).ready(function() {
+    // your function, copied 100%
+    function setIndex() {
+        $("td.index").each(function(index) {
+            $(this).text(++index);
+        });
+    }
+
+    // set the index to begin with. Note the last three 
+    // row indexes are actually empty in the sample HTML
+    setIndex();
+
+    // Move the click listener to the table. 
+    $('table').on('click', '.delete-button', function() {
+        // remove the tr...
+        $(this).parents('tr').remove();
+        //... and reset the index
+        setIndex();
+    })
+})
+
+function bestDivisor(n) {
+    var arr = [];
+    var k = 0;
+    for (let i = 1; i <= n / 2; i++) {
+        if (n % i == 0) arr.push(i);
+    }
+    arr.push(n);
+
+    if (Math.max.apply(Math, arr) < 10) k = Math.max.apply(Math, arr);
+    else {
+        k = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+            if (di(k) < di(arr[i])) {
+                k = arr[i];
+            }
+        }
+    }
+
+    return k;
+
+}
+
+function di(a) {
+    var sum = 0;
+    while (a > 0) {
+        sum += a % 10;
+        a -= a % 10;
+        a /= 10;
+    }
+    return Math.floor(sum);
+}
